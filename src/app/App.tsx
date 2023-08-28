3620c7ef29f72a107473469a2b01a427357ad053
malihgh <malih.ghasemian@gmail.com>
2023-08-28 18:29:42 +0200

    💄 feat : Add style for updating body in App, and using Home.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
 import * as Styled from "./styles";
 import StyledThemeProvider from "core/provider/ThemeProvider/ThemeProvider";
+import Home from "pages/Home/Home";
 
 function App() {
   return (
     <StyledThemeProvider>
+      <style>{`
+        body {
+          margin: 0px;
+          padding: 0px;
+        }
+      `}</style>
       <Styled.Container>
-        <div>Hello</div>
+        <Home />
       </Styled.Container>
     </StyledThemeProvider>
   );
