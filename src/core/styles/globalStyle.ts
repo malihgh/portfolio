27736cbd298c74bcb35f8ec5f18fb5fec3577e86
malihgh <malih.ghasemian@gmail.com>
2023-08-28 21:37:50 +0200

    feat : Add @font-face for Bold-font.

--- src/core/styles/globalStyle.ts
+++ src/core/styles/globalStyle.ts
@@ -1,7 +1,8 @@
-import {createGlobalStyle} from 'styled-components';
-import Poppins from 'assets/fonts/Poppins-Regular.woff';
-import Poppins2 from 'assets/fonts/Poppins-Regular.woff2';
-
+import { createGlobalStyle } from "styled-components";
+import Poppins from "assets/fonts/Poppins-Regular.woff";
+import Poppins2 from "assets/fonts/Poppins-Regular.woff2";
+import PoppinsBold from "assets/fonts/Poppins-Bold.woff";
+import PoppinsBold2 from "assets/fonts/Poppins-Bold.woff2";
 
 export default createGlobalStyle`
     @font-face {
@@ -9,6 +10,14 @@ export default createGlobalStyle`
         src: local('Poppins'), local('Poppins'),
         url(${Poppins}) format('woff'),
         url(${Poppins2}) format('woff2');
+        font-weight: 300;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Poppins-Bold';
+        src: local('PoppinsBold'), local('PoppinsBold'),
+        url(${PoppinsBold}) format('woff'),
+        url(${PoppinsBold2}) format('woff2');
         font-weight: 300;
         font-style: normal;
     }
