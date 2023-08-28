9952dba7b6519723d069363029b6c6debe6a9db6
malihgh <malih.ghasemian@gmail.com>
2023-08-28 20:02:12 +0200

    feat : Update theme for prevent capitalizing text in Button.

--- src/core/styles/theme.ts
+++ src/core/styles/theme.ts
@@ -1,6 +1,11 @@
 import { createTheme } from "@mui/material";
 
 export const customTheme = createTheme({
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
   palette: {
     primary: {
       main: "#2FBFBF",
