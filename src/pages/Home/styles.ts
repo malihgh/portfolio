3a568b4757ab05df18910d34616fa154c850058c
malihgh <malih.ghasemian@gmail.com>
2023-08-28 18:30:02 +0200

    feat : Add background-image for Home.

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,13 @@
 import styled from "@emotion/styled";
 import { MuiEmotion } from "core/types/MuiEmotion";
+import background from "assets/Images/home-background.png";
 
 export const Container = styled.div<MuiEmotion>`
   font-family: "Poppins", sans-serif;
   background-color: ${({ theme }) => theme.palette.primary.main};
   height: 100vh;
+  opacity: 14%;
+  background-image: url(${background});
+  background-size: contain; //cover
+  background-repeat: no-repeat;
 `;
