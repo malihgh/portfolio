0953ab4fb4252d1d632339c751005c94be6619b2
malihgh <malih.ghasemian@gmail.com>
2023-08-29 18:34:39 +0200

    feat : Make Expertise component and add List for it.

--- src/pages/Skill/components/Expertise/Expertise.tsx
+++ src/pages/Skill/components/Expertise/Expertise.tsx
@@ -1,7 +1,24 @@
 import * as Styled from "./styles";
+import { ExpertiseList } from "./ExpertiseList";
 
 const Expertise = () => {
-  return <Styled.Container>Expertise</Styled.Container>;
+  return (
+    <Styled.Container>
+      <Styled.TitleContainer>
+        <Styled.Title>Expertise</Styled.Title>
+      </Styled.TitleContainer>
+
+      {ExpertiseList.map((category) => (
+        <Styled.Category>
+          {category.items.map((item) => (
+            <Styled.Expertise>
+              <Styled.Text>{item}</Styled.Text>
+            </Styled.Expertise>
+          ))}
+        </Styled.Category>
+      ))}
+    </Styled.Container>
+  );
 };
 
 export default Expertise;
--- src/pages/Skill/components/Expertise/styles.ts
+++ src/pages/Skill/components/Expertise/styles.ts
@@ -3,4 +3,49 @@ import { MuiEmotion } from "core/types/MuiEmotion";
 
 export const Container = styled.div<MuiEmotion>`
   flex: 1;
+  padding-left: 40px;
+  padding-bottom: 20px;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const Title = styled.div<MuiEmotion>`
+  color: ${({ theme }) => theme.palette.text.primary}; /* color: #4a4a4a; */
+  text-align: center;
+  font-family: "Poppins-Bold", sans-serif;
+  font-size: 32px;
+  font-style: normal;
+  line-height: 48px;
+  margin-bottom: 20px;
+`;
+
+export const Category = styled.div`
+  margin-bottom: 15px;
+`;
+
+export const Expertise = styled.div<MuiEmotion>`
+  display: inline-block;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 7px;
+  border-radius: 6px;
+  /* background: ${({ theme }) => theme.palette.primary.light}; */
+  background: #eaf3f3; //color
+  margin-right: 10px;
+  margin-bottom: 7px;
+`;
+
+export const Text = styled.div<MuiEmotion>`
+  color: #5b5b5b; //color: #5b5b5b
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 28px;
 `;
