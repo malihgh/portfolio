47280d779aeef82630d7d75a3256e6de959d1e8a
malihgh <malih.ghasemian@gmail.com>
2023-08-29 17:46:11 +0200

    feat : Add KeyExpertise Component and complete it.

---
+++ src/pages/Skill/components/KeyExpertise/KeyExpertiseList.tsx
@@ -0,0 +1,40 @@
+export const KeyExpertiseList = [
+  {
+    id: 0,
+    title: (
+      <>
+        <strong>React</strong>, <strong>TypeScript</strong> and{" "}
+        <strong>HTML5, CSS3, JS</strong>
+      </>
+    ),
+  },
+  {
+    id: 1,
+    title: (
+      <>
+        <strong>Object Oriented</strong> design
+      </>
+    ),
+  },
+  {
+    id: 2,
+    title: (
+      <>
+        <strong>UI/UX</strong> processes and <strong>Agile</strong>
+      </>
+    ),
+  },
+  {
+    id: 3,
+    title: (
+      <>
+        Familiar with <strong>Design Patterns</strong> and{" "}
+        <strong>Clean Code</strong>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    title: <>Good at teamwork, problem solving and math</>,
+  },
+];
---
+++ src/pages/Skill/components/KeyExpertise/styles.ts
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import { MuiEmotion } from "core/types/MuiEmotion";
+
+export const Container = styled.div<MuiEmotion>`
+  flex: 1;
+`;
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+export const Title = styled.div<MuiEmotion>`
+  color: ${({ theme }) => theme.palette.text.primary}; /* color: #4a4a4a; */
+  text-align: center;
+  font-family: "Poppins-Bold", sans-serif;
+  font-size: 35px;
+  font-style: normal;
+  line-height: 48px;
+  margin-bottom: 30px;
+`;
+
+export const Expertise = styled.div<MuiEmotion>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 12px;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.palette.primary.light};
+  /* background: #eaf3f3; */
+  margin-bottom: 10px;
+`;
+
+export const Arrow = styled.img`
+  width: 25px;
+  margin-right: 12px;
+`;
+
+export const Text = styled.div<MuiEmotion>`
+  color: #5b5b5b; //color: #5b5b5b
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 28px;
+
+  strong {
+    font-weight: 600;
+  }
+`;
