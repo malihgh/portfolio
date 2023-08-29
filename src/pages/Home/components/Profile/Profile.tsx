bbd084f17d132c0eff419f7d34395f3381df310d
malihgh <malih.ghasemian@gmail.com>
2023-08-29 15:21:23 +0200

    feat : Add starts and shapes to Profile.

--- src/pages/Home/components/Profile/Profile.tsx
+++ src/pages/Home/components/Profile/Profile.tsx
@@ -1,8 +1,14 @@
 import * as Styled from "./styles";
 import profile from "assets/Images/profile.png";
+import starTop from "assets/Images/star-big-green.png";
+import starBottom from "assets/Images/start-samll.png";
+
 const Profile = () => {
   return (
     <Styled.ProfileContainer>
+      <Styled.StarTop src={starTop} />
+      <Styled.StarBottom src={starBottom} />
+      <Styled.Circle />
       <Styled.Frame>
         <Styled.Profile src={profile} />
       </Styled.Frame>
--- src/pages/Home/components/Profile/styles.tsx
+++ src/pages/Home/components/Profile/styles.tsx
@@ -9,7 +9,7 @@ export const ProfileContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
+  position: relative;
   @media only screen and (max-width: 740px) {
     display: none; //fix it later
   }
@@ -21,8 +21,9 @@ export const Frame = styled.div<MuiEmotion>`
   flex-shrink: 0;
   border-radius: ${profileWidth + "px"};
   margin: 30px 0 0 30px;
-  /* border: ${({ theme }) => `2xp solid ${theme.palette.secondary.main}`}; */
-  border: 2px solid white; //fix it
+  ${({ theme }) => `
+    border: 2px solid ${theme.palette.text.secondary};
+  `}
   display: flex;
   justify-content: center;
   align-items: center;
@@ -33,4 +34,30 @@ export const Profile = styled.img<MuiEmotion>`
   height: ${profileHeight + "px"};
   flex-shrink: 0;
   border-radius: ${profileWidth + "px"};
+`;
+
+export const StarTop = styled.img<MuiEmotion>`
+  width: 75px;
+  position: absolute;
+  top: 30px;
+  right: 17px;
+`;
+
+export const StarBottom = styled.img<MuiEmotion>`
+  width: 30px;
+  position: absolute;
+  left: 50px;
+  bottom: 44px;
+`;
+
+export const Circle = styled.div<MuiEmotion>`
+  width: 10px;
+  height: 10px;
+  ${({ theme }) => `
+    border: 1px solid ${theme.palette.secondary.main};
+  `}
+  border-radius: 50%;
+  position: absolute;
+  right: 19px;
+  bottom: 70px;
 `;
