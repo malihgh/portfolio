b89132807d4a2755190e120c96d2e4333ac646bf
malihgh <malih.ghasemian@gmail.com>
2023-08-29 17:59:46 +0200

    feat : Make Expertise.

---
+++ src/pages/Skill/components/Expertise/Expertise.tsx
@@ -0,0 +1,7 @@
+import * as Styled from "./styles";
+
+const Expertise = () => {
+  return <Styled.Container>Expertise</Styled.Container>;
+};
+
+export default Expertise;
---
+++ src/pages/Skill/components/Expertise/styles.ts
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import { MuiEmotion } from "core/types/MuiEmotion";
+
+export const Container = styled.div<MuiEmotion>`
+  flex: 1;
+`;
