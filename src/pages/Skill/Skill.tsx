2a531d1119736614e3ae82868b66ef1be14dcf38
malihgh <malih.ghasemian@gmail.com>
2023-08-29 15:52:54 +0200

    feat : Make Skill component.

---
+++ src/pages/Skill/Skill.tsx
@@ -0,0 +1,7 @@
+import * as Styled from "./styles";
+
+const Skill = () => {
+  return <Styled.SkillContainer>Skill</Styled.SkillContainer>;
+};
+
+export default Skill;
---
+++ src/pages/Skill/styles.ts
@@ -0,0 +1,4 @@
+import styled from "@emotion/styled";
+import { MuiEmotion } from "core/types/MuiEmotion";
+
+export const SkillContainer = styled.div<MuiEmotion>``;
