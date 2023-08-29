95fed4f9dab606db2c4d7a606eab928c949f64c5
malihgh <malih.ghasemian@gmail.com>
2023-08-29 16:05:57 +0200

    feat : Add title and subtitle for Skills page.

--- src/pages/Skill/Skill.tsx
+++ src/pages/Skill/Skill.tsx
@@ -1,7 +1,15 @@
 import * as Styled from "./styles";
 
 const Skill = () => {
-  return <Styled.SkillContainer>Skill</Styled.SkillContainer>;
+  return (
+    <Styled.SkillContainer>
+      <Styled.Title>My Skills</Styled.Title>
+      <Styled.SubTitle>
+        Here you will find more information, what I do, and my current skills
+        mostly in terms of programming and technology
+      </Styled.SubTitle>
+    </Styled.SkillContainer>
+  );
 };
 
 export default Skill;
--- src/pages/Skill/styles.ts
+++ src/pages/Skill/styles.ts
@@ -2,3 +2,25 @@ import styled from "@emotion/styled";
 import { MuiEmotion } from "core/types/MuiEmotion";
 
 export const SkillContainer = styled.div<MuiEmotion>``;
+
+export const Title = styled.div<MuiEmotion>`
+  color: ${({ theme }) => theme.palette.text.primary};
+  text-align: center;
+  font-family: "Poppins-Bold", sans-serif;
+  font-size: 44px;
+  font-style: normal;
+  line-height: 48px;
+  margin-top: 90px;
+`;
+export const SubTitle = styled.div`
+  color: #6c6c6c; //fix
+  text-align: center;
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  width: 50%;
+  margin: auto;
+  margin-top: 17px;
+`;
