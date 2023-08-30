ff68d78cececc000157a40ad4834efb6eb123fed
malihgh <malih.ghasemian@gmail.com>
2023-08-30 10:50:19 +0200

    feat : Add background to About.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,11 @@
 import * as Styled from "./styles";
 
 const About = () => {
-  return <Styled.AboutContainer>About</Styled.AboutContainer>;
+  return (
+    <Styled.AboutContainer>
+      <Styled.Row></Styled.Row>
+    </Styled.AboutContainer>
+  );
 };
 
 export default About;
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,4 +1,22 @@
 import styled from "@emotion/styled";
 import { MuiEmotion } from "core/types/MuiEmotion";
+import background from "assets/images/about-background.png";
 
-export const AboutContainer = styled.div<MuiEmotion>``;
+export const AboutContainer = styled.div<MuiEmotion>`
+  height: 100vh;
+  background-image: url(${background});
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  padding: 0px 85px;
+
+  @media only screen and (max-width: 740px) {
+    padding: 0px 55px;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
