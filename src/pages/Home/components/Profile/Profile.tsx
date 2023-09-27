33f930d9cc95267b33f21436010bfc708bf4313e
malihgh <malih.ghasemian@gmail.com>
2023-09-27 19:24:20 +0200

    feat : Update image name.

--- src/pages/Home/components/Profile/Profile.tsx
+++ src/pages/Home/components/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import * as Styled from "./styles";
 import profile from "assets/images/profile.png";
 import starTop from "assets/images/star-big-green.png";
-import starBottom from "assets/images/start-samll.png";
+import starBottom from "assets/images/start-small.png";
 
 const Profile = () => {
   return (
