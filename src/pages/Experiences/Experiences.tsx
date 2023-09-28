c912f342f3ed7e67aa070866671274c2e13bdbad
malihgh <malih.ghasemian@gmail.com>
2023-09-28 17:33:53 +0200

    feat : Make Experience right colomn.

--- src/pages/Experiences/Experiences.tsx
+++ src/pages/Experiences/Experiences.tsx
@@ -1,7 +1,24 @@
 import * as Styled from "./styles";
+import HeaderWithIcon from "core/components/HeaderWithIcon";
+import Pointer from "assets/images/pointer-long-yellow.png";
 
 const Experiences = () => {
-  return <Styled.ExperiencesContainer>Experiences</Styled.ExperiencesContainer>;
+  return (
+    <Styled.ExperiencesContainer>
+      <Styled.LeftPart>
+        <div>
+          <HeaderWithIcon
+            title="My Experience"
+            subTitle="I have had the pleasure to work with companies across a variety of industries. I'm always interested in new, exciting and challenging adventures."
+          />
+          <Styled.CVButton title="Download CV" />
+        </div>
+
+        <Styled.Pointer src={Pointer} />
+      </Styled.LeftPart>
+      <Styled.RightPart>right</Styled.RightPart>
+    </Styled.ExperiencesContainer>
+  );
 };
 
 export default Experiences;
--- src/pages/Experiences/styles.ts
+++ src/pages/Experiences/styles.ts
@@ -1,3 +1,34 @@
+import Button from "core/components/Button";
 import styled from "styled-components";
 
-export const ExperiencesContainer = styled.div``;
+export const ExperiencesContainer = styled.div`
+  height: 100vh;
+  padding: 0px 85px;
+  display: flex;
+
+  @media only screen and (max-width: 740px) {
+    padding: 0px 55px 20px 55px;
+    height: auto;
+  }
+`;
+
+export const LeftPart = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const RightPart = styled.div`
+  flex: 1;
+`;
+
+export const Pointer = styled.img`
+  width: 178px;
+  height: 235px;
+`;
+
+export const CVButton = styled(Button)`
+  margin-top: 40px;
+`;
