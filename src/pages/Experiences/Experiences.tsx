816643a9037c35a446b92bdbc4fa450fc96a837c
malihgh <malih.ghasemian@gmail.com>
2023-09-29 20:05:26 +0200

    feat : Fix styles for Experiences leftPart.

--- src/pages/Experiences/Experiences.tsx
+++ src/pages/Experiences/Experiences.tsx
@@ -5,18 +5,29 @@ import Pointer from "assets/images/pointer-long-yellow.png";
 const Experiences = () => {
   return (
     <Styled.ExperiencesContainer>
-      <Styled.LeftPart>
-        <div>
-          <HeaderWithIcon
-            title="My Experience"
-            subTitle="I have had the pleasure to work with companies across a variety of industries. I'm always interested in new, exciting and challenging adventures."
-          />
-          <Styled.CVButton title="Download CV" />
-        </div>
+      <Styled.Row>
+        <Styled.LeftPart>
+          <div>
+            <HeaderWithIcon
+              title="My Experience"
+              subTitle="I have had the pleasure to work with companies across a variety of industries. I'm always interested in new, exciting and challenging adventures."
+              titleStyle={{
+                textAlign: "left",
+                paddingLeft: 0,
+              }}
+              subTitleStyle={{
+                textAlign: "left",
+                marginLeft: 0,
+                marginBottom: "30px",
+              }}
+            />
+            <Styled.CVButton title="Download CV" />
+          </div>
 
-        <Styled.Pointer src={Pointer} />
-      </Styled.LeftPart>
-      <Styled.RightPart>right</Styled.RightPart>
+          <Styled.Pointer src={Pointer} />
+        </Styled.LeftPart>
+        <Styled.RightPart>right</Styled.RightPart>
+      </Styled.Row>
     </Styled.ExperiencesContainer>
   );
 };
--- src/pages/Experiences/styles.ts
+++ src/pages/Experiences/styles.ts
@@ -1,23 +1,16 @@
-import Button from "core/components/Button";
 import styled from "styled-components";
+import Button from "core/components/Button";
+import ScreenContainer from "core/components/ScreenContainer";
 
-export const ExperiencesContainer = styled.div`
-  height: 100vh;
-  padding: 0px 85px;
+export const ExperiencesContainer = styled(ScreenContainer)``;
+
+export const Row = styled.div`
   display: flex;
-
-  @media only screen and (max-width: 740px) {
-    padding: 0px 55px 20px 55px;
-    height: auto;
-  }
+  flex: 1;
+  justify-content: space-between;
 `;
-
 export const LeftPart = styled.div`
   flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
 `;
 
 export const RightPart = styled.div`
@@ -27,8 +20,10 @@ export const RightPart = styled.div`
 export const Pointer = styled.img`
   width: 178px;
   height: 235px;
+  padding-top: 90px;
+  padding-left: 60px;
 `;
 
 export const CVButton = styled(Button)`
-  margin-top: 40px;
+  //style dosen't work!!!-> add margin to subtitle above button
 `;
