6202a6c1e2deb2e632a6520dee006d4b8d1d1f29
malihgh <malih.ghasemian@gmail.com>
2023-09-29 19:04:44 +0200

    feat : Update components with ScreenContainer.

--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,23 +1,12 @@
 import styled from "@emotion/styled";
 import { MuiEmotion } from "core/types/MuiEmotion";
 import background from "assets/images/about-background.png";
+import ScreenContainer from "core/components/ScreenContainer";
 
-export const AboutContainer = styled.div<MuiEmotion>`
-  height: 100vh;
+export const AboutContainer = styled(ScreenContainer)<MuiEmotion>`
   background-image: url(${background});
   background-size: 100% 100%;
   background-repeat: no-repeat;
-  padding: 0px 85px;
-
-  @media only screen and (max-width: 740px) {
-    padding: 0px 55px 20px 55px;
-    height: auto;
-  }
-
-  @media only screen and (max-width: 1024px) {
-    height: auto;
-    padding-bottom: 20px;
-  }
 `;
 
 export const Row = styled.div`
