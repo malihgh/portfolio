7e76db652725f14cb1963af51f4198a44a843ca8
malihgh <malih.ghasemian@gmail.com>
2023-10-02 11:01:11 +0200

    feat : Make complete ExperienceBox.

--- src/pages/Experiences/components/ExperienceBox/styles.ts
+++ src/pages/Experiences/components/ExperienceBox/styles.ts
@@ -2,4 +2,8 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
