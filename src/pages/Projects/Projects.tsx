66b7323872a06f46e3c15f2ad2487c96649f7689
malihgh <malih.ghasemian@gmail.com>
2023-10-09 15:54:44 +0200

    feat : Add Projects header with HeaderWithIcon.

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,7 +1,21 @@
+import HeaderWithIcon from "core/components/HeaderWithIcon";
+import excited from "assets/images/excited-shape-black.png";
+
 import * as Styled from "./styles";
 
 const Projects = () => {
-  return <Styled.ProjectsContainer>Projects</Styled.ProjectsContainer>;
+  return (
+    <Styled.ProjectsContainer>
+      <HeaderWithIcon
+        title="Projects"
+        subTitle="Here you will find some of the personal and clients projects that I created with each project containing its own case study"
+        titleStyle={{
+          margin: "auto",
+        }}
+      />
+      <Styled.Shape src={excited} />
+    </Styled.ProjectsContainer>
+  );
 };
 
 export default Projects;
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,4 +1,20 @@
 import styled from "@emotion/styled";
 import ScreenContainer from "core/components/ScreenContainer";
 
-export const ProjectsContainer = styled(ScreenContainer)``;
+export const ProjectsContainer = styled(ScreenContainer)`
+  position: relative;
+  background-color: #fffcf3;
+`;
+
+export const Shape = styled.img`
+  width: 150px;
+  rotate: 6deg;
+  position: absolute;
+  top: 70px;
+  right: 80px;
+
+  @media only screen and (max-width: 740px) {
+    top: -40px;
+    right: 0px;
+  }
+`;
