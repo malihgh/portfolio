ca857f5aeaa74a204b02ca2534216ba05dfd1b62
malihgh <malih.ghasemian@gmail.com>
2023-10-18 14:16:51 +0200

    feat : Add ProjectsData json.

---
+++ src/pages/Projects/data/ProjectsData.ts
@@ -0,0 +1,45 @@
+export type ProjectsItemType = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+};
+
+export const projectsData: ProjectsItemType[] = [
+  {
+    id: 0,
+    title: "Gespos",
+    description: "Managing geomatics surveys collected in Canton Ticino",
+    image: "src/assets/images/profile.png",
+  },
+  {
+    id: 1,
+    title: "Swissforages",
+    description: "Platform to acquire bore- hole data by Swisstopo",
+    image: "src/assets/images/profile.png",
+  },
+  {
+    id: 2,
+    title: "Mix Or Match game",
+    description: "Memory game with a twist",
+    image: "src/assets/images/profile.png",
+  },
+  {
+    id: 3,
+    title: "Connected Home Care",
+    description: "For the elderly and their caregivers",
+    image: "src/assets/images/profile.png",
+  },
+  {
+    id: 4,
+    title: " go! Fleet Driver and Cockpit",
+    description: "For the drivers of the go! fleet",
+    image: "src/assets/images/profile.png",
+  },
+  {
+    id: 5,
+    title: "AutoTik",
+    description: "For keep track of your car maintenance",
+    image: "src/assets/images/profile.png",
+  },
+];
