c1a4ad1cf4c98e980352d662e22e4b2fc89fbdc6
malihgh <malih.ghasemian@gmail.com>
2023-10-18 14:18:54 +0200

    feat : Call ProjectCards by ProjectsData in ProjectList.

--- src/pages/Projects/components/ProjectList/ProjectList.tsx
+++ src/pages/Projects/components/ProjectList/ProjectList.tsx
@@ -1,14 +1,28 @@
-import ProjectCard from "../ProjectCard";
 import * as Styled from "./styles";
+import ProjectCard from "../ProjectCard";
+import {
+  projectsData,
+  ProjectsItemType,
+} from "pages/Projects/data/ProjectsData";
 
 const ProjectList = () => {
+  function splitArray(arr: ProjectsItemType[], chunkSize: number) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      result.push(arr.slice(i, i + chunkSize));
+    }
+    return result;
+  }
+  const chunkedProjectsData = splitArray(projectsData, 3);
   return (
     <Styled.Container>
-      <Styled.List>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-      </Styled.List>
+      {chunkedProjectsData.map((groups) => (
+        <Styled.List>
+          {groups.map((project) => (
+            <ProjectCard {...project} />
+          ))}
+        </Styled.List>
+      ))}
     </Styled.Container>
   );
 };
