d836ee7119ea437716a6fd07898493867d473708
malihgh <malih.ghasemian@gmail.com>
2023-10-18 10:30:49 +0200

    feat : Make ProjectCard ready to use.

--- src/pages/Projects/components/ProjectCard/ProjectCard.tsx
+++ src/pages/Projects/components/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,27 @@
 import * as Styled from "./styles";
+import profile from "assets/images/profile.png";
 
 const ProjectCard = () => {
-  return <Styled.Card>ProjectCard</Styled.Card>;
+  const projectDetails = {
+    title: "Project Title",
+    description: "Project Description",
+    image: profile,
+  };
+
+  return (
+    <Styled.Card>
+      <Styled.ImageFrame>
+        <Styled.Image src={projectDetails.image} />
+      </Styled.ImageFrame>
+
+      <Styled.InfoContainer>
+        <Styled.Title>{projectDetails.title}</Styled.Title>
+        <Styled.Description>{projectDetails.description}</Styled.Description>
+
+        {/* Add a details page and link it to this button--> arrow-up.svg icon */}
+      </Styled.InfoContainer>
+    </Styled.Card>
+  );
 };
 
 export default ProjectCard;
--- src/pages/Projects/components/ProjectCard/styles.ts
+++ src/pages/Projects/components/ProjectCard/styles.ts
@@ -1,3 +1,57 @@
 import styled from "@emotion/styled";
 
-export const Card = styled.div``;
+export const Card = styled.div`
+  margin-top: 25px;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const ImageFrame = styled.div`
+  background-color: #edf0ee;
+  width: 90%;
+  height: 225px;
+  border-radius: 15px;
+  overflow: hidden;
+  margin-bottom: 10px;
+  display: flex;
+`;
+
+// export const ImageBackground = styled.div`
+//   width: 100%;
+//   background-image: url(${profile});
+//   background-size: auto 100%;
+//   background-repeat: no-repeat;
+// `; //for show image with right size
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const InfoContainer = styled.div`
+  width: 90%;
+`;
+
+export const Title = styled.div`
+  color: #303535;
+  font-family: "Poppins", sans-serif;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  text-transform: capitalize;
+`;
+
+export const Description = styled.div`
+  color: #646060;
+  font-family: "Poppins", sans-serif;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-transform: capitalize;
+`;
