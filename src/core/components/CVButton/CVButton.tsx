393a846a271d32ccaed45e2fc79b9040b1dbf999
malihgh <malih.ghasemian@gmail.com>
2023-10-19 11:05:12 +0200

    feat : Update CVButton.

--- src/core/components/CVButton/CVButton.tsx
+++ src/core/components/CVButton/CVButton.tsx
@@ -1,13 +1,22 @@
+import { FC } from "react";
+import { ButtonProps } from "@mui/material";
 import Button from "core/components/Button";
 
-const CVButton = () => {
+const CVButton: FC<ButtonProps> = (props) => {
   const openURL = () => {
     const url =
       "https://drive.google.com/file/d/1J94SCHMsKeu4oFiA3cyS_LotEQBJd5O4/view";
     window.open(url, "_blank", "noreferrer");
   };
 
-  return <Button title="Download CV" onClick={() => openURL()} />;
+  return (
+    <Button
+      title="Download CV"
+      onClick={() => openURL()}
+      {...props}
+      color="primary"
+    />
+  );
 };
 
 export default CVButton;
