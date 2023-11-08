b19a08c747d12f7ce5fc859163bf091eb73feafb
malihgh <malih.ghasemian@gmail.com>
2023-11-08 18:07:06 +0100

    feat : Create PageListData.

--- src/pages/Home/components/Header/Header.tsx
+++ src/pages/Home/components/Header/Header.tsx
@@ -1,19 +1,12 @@
 import * as Styled from "./styles";
+import { pagesListData } from "pages/Home/data/PageListData";
 import { HashLink } from "react-router-hash-link";
 
-const pages = [
-  { id: 0, name: "Home", path: "#home" },
-  { id: 1, name: "My skills", path: "#skill" },
-  { id: 2, name: "About", path: "#about" },
-  { id: 3, name: "Experience", path: "#experiences" },
-  { id: 4, name: "projects", path: "#projects" },
-  { id: 5, name: "Contact", path: "/" },
-];
 const Header = () => {
   return (
     <Styled.HeaderContainer>
       <Styled.Name>Maliheh</Styled.Name>
-      {pages.map((page) => (
+      {pagesListData.map((page) => (
         <Styled.Pages key={page.id}>
           <HashLink smooth to={page.path} style={{ textDecoration: "none" }}>
             <Styled.PageName active={page.id === 0}>
