874c7fa33547480b877dd8053dbe13e7c9a8368c
malihgh <malih.ghasemian@gmail.com>
2023-11-16 20:09:43 +0100

    feat : Update images for ProjectCard bsc of the error in deployment.

--- src/pages/Projects/components/ProjectCard/ProjectCard.tsx
+++ src/pages/Projects/components/ProjectCard/ProjectCard.tsx
@@ -1,14 +1,35 @@
 import { FC } from "react";
 import * as Styled from "./styles";
 import { ProjectsItemType } from "pages/Projects/data/ProjectsData";
+import gespos from "assets/images/projects/gespos.jpeg";
+import swissforages from "assets/images/projects/swissforages.png";
+import mixOrMatch from "assets/images/projects/mix-or-match.png";
+import connectedHomeCare from "assets/images/projects/connect-home-care.png";
+import cockpit from "assets/images/projects/cockpit.jpg";
+import goFleet from "assets/images/projects/go-fleet.png";
+import autoTik from "assets/images/projects/autotik.png";
+import limoRestaurant from "assets/images/projects/limo-restaurant.png";
+import asena from "assets/images/projects/asena.png";
 
 const ProjectCard: FC<ProjectsItemType> = (props) => {
-  const { title, description, image, isApp } = props;
+  const { id, title, description, isApp } = props;
 
+  //same order as the data in ProjectsData.ts
+  const images = [
+    gespos,
+    swissforages,
+    mixOrMatch,
+    connectedHomeCare,
+    cockpit,
+    goFleet,
+    autoTik,
+    limoRestaurant,
+    asena,
+  ];
   return (
     <Styled.Card>
       <Styled.ImageFrame>
-        <Styled.Image src={image} alt="images" isApp={isApp} />
+        <Styled.Image src={images[id]} alt="images" isApp={isApp} />
       </Styled.ImageFrame>
 
       <Styled.InfoContainer>
--- src/pages/Projects/data/ProjectsData.ts
+++ src/pages/Projects/data/ProjectsData.ts
@@ -2,7 +2,7 @@ export type ProjectsItemType = {
   id: number;
   title: string;
   description: string;
-  image: string;
+  // image: string;//fix it later
   isApp: boolean;
 };
 
@@ -11,63 +11,54 @@ export const projectsData: ProjectsItemType[] = [
     id: 0,
     title: "Gespos",
     description: "Managing geomatics surveys collected in Canton Ticino",
-    image: "src/assets/images/projects/gespos.jpeg",
     isApp: false,
   },
   {
     id: 1,
     title: "Swissforages",
     description: "Platform to acquire borehole data by Swisstopo",
-    image: "src/assets/images/projects/swissforages.png",
     isApp: false,
   },
   {
     id: 2,
     title: "Mix Or Match game",
     description: "Memory game with a twist",
-    image: "src/assets/images/projects/mix-or-match.png",
     isApp: false,
   },
   {
     id: 3,
     title: "Connected Home Care",
     description: "For the elderly and their caregivers",
-    image: "src/assets/images/projects/connect-home-care.png",
     isApp: true,
   },
   {
     id: 4,
     title: "Cockpit Website",
     description: "For connecting passengers and drivers of the go! fleet",
-    image: "src/assets/images/projects/cockpit.jpg",
     isApp: false,
   },
   {
     id: 5,
     title: "go! Fleet Driver App",
     description: "For the drivers of the go! fleet",
-    image: "src/assets/images/projects/go-fleet.png",
     isApp: true,
   },
   {
     id: 6,
     title: "AutoTik",
     description: "For keep track of your car maintenance",
-    image: "src/assets/images/projects/autotik.png",
     isApp: true,
   },
   {
     id: 7,
     title: "Limo Restaurant",
     description: "For ordering food online",
-    image: "src/assets/images/projects/limo-restaurant.png",
     isApp: false,
   },
   {
     id: 8,
     title: "Asena",
     description: "Time Tracking App",
-    image: "src/assets/images/projects/asena.png",
     isApp: true,
   },
 ];
