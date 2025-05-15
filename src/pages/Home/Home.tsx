0dafe1de18ec7fb7b431c97ef1baa3c1f3064ade
malihgh <malih.ghasemian@gmail.com>
2025-05-15 16:37:25 +0200

    feat: Remove Header from Home component.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,10 @@
 import * as Styled from "./styles";
-import Header from "pages/Home/components/Header";
 import Intro from "pages/Home/components/Intro";
 import Profile from "./components/Profile";
 
 const Home = () => {
   return (
     <Styled.Container>
-      <Header />
-
       <Styled.Row>
         <Intro />
         <Profile />
