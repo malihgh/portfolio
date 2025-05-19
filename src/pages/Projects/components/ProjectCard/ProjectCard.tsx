2ce0aa0ce668434c6804b6f6d9e4644b99b12985
malihgh <malih.ghasemian@gmail.com>
2025-05-19 14:37:47 +0200

    feat: Fix issue for images as a props on ProjectCard.

--- src/pages/Projects/components/ProjectCard/ProjectCard.tsx
+++ src/pages/Projects/components/ProjectCard/ProjectCard.tsx
@@ -1,37 +1,9 @@
+import { ProjectsItemType } from "pages/Projects/data/ProjectsData";
 import { FC } from "react";
 import * as Styled from "./styles";
-import { ProjectsItemType } from "pages/Projects/data/ProjectsData";
-import kitchenaid from "assets/images/projects/kitchenaid.png";
-import pokemon from "assets/images/projects/pokemon.png";
-import practicaMente from "assets/images/projects/practica-mente.png";
-import gespos from "assets/images/projects/gespos.jpeg";
-import swissforages from "assets/images/projects/swissforages.png";
-import mixOrMatch from "assets/images/projects/mix-or-match.png";
-import connectedHomeCare from "assets/images/projects/connect-home-care.png";
-import cockpit from "assets/images/projects/cockpit.jpg";
-import goFleet from "assets/images/projects/go-fleet.png";
-import autoTik from "assets/images/projects/autotik.png";
-import limoRestaurant from "assets/images/projects/limo-restaurant.png";
-import asena from "assets/images/projects/asena.png";
 
 const ProjectCard: FC<ProjectsItemType> = (props) => {
-  const { id, title, description, isApp = false, url } = props;
-
-  //same order as the data in ProjectsData.ts
-  const images = [
-    kitchenaid,
-    pokemon,
-    practicaMente,
-    gespos,
-    swissforages,
-    mixOrMatch,
-    connectedHomeCare,
-    cockpit,
-    goFleet,
-    autoTik,
-    limoRestaurant,
-    asena,
-  ];
+  const { title, description, isApp = false, url, image } = props;
 
   const openUrl = () => {
     if (url) {
@@ -41,7 +13,7 @@ const ProjectCard: FC<ProjectsItemType> = (props) => {
   return (
     <Styled.Card hasUrl={!!url} onClick={openUrl}>
       <Styled.ImageFrame>
-        <Styled.Image src={images[id]} alt="images" isApp={isApp} />
+        <Styled.Image src={image} alt={title} isApp={isApp} />
       </Styled.ImageFrame>
 
       <Styled.InfoContainer>
--- src/pages/Projects/data/ProjectsData.ts
+++ src/pages/Projects/data/ProjectsData.ts
@@ -1,8 +1,21 @@
+import asena from "assets/images/projects/asena.png";
+import autoTik from "assets/images/projects/autotik.png";
+import cockpit from "assets/images/projects/cockpit.jpg";
+import connectedHomeCare from "assets/images/projects/connect-home-care.png";
+import gespos from "assets/images/projects/gespos.jpeg";
+import goFleet from "assets/images/projects/go-fleet.png";
+import kitchenaid from "assets/images/projects/kitchenaid.png";
+import limoRestaurant from "assets/images/projects/limo-restaurant.png";
+import mixOrMatch from "assets/images/projects/mix-or-match.png";
+import pokemon from "assets/images/projects/pokemon.png";
+import practicaMente from "assets/images/projects/practica-mente.png";
+import swissforages from "assets/images/projects/swissforages.png";
+
 export type ProjectsItemType = {
   id: number;
   title: string;
   description: string;
-  // image: string;//fix it later
+  image: string;
   url?: string;
   isApp?: boolean;
 };
@@ -14,35 +27,41 @@ export const projectsData: ProjectsItemType[] = [
     description:
       "A global e-commerce website for KitchenAid, an American appliance brand owned by Whirlpool",
     url: "https://www.kitchenaid.ie/",
+    image: kitchenaid,
   },
   {
     id: 1,
     title: "Pokèmon",
     description: "Simulation of pokemon website with the help of pokeapi.co",
     url: "https://mali-pokemon.vercel.app/",
+    image: pokemon,
   },
   {
     id: 2,
     title: "Practica-mente website",
     description: "A bilingual website for a professional coach using Next.js",
     url: "https://practica-mente.vercel.app/en",
+    image: practicaMente,
   },
   {
     id: 3,
     title: "Gespos",
     description: "Managing geomatics surveys collected in Canton Ticino",
     url: "https://www.ist.supsi.ch/portale-gespos-ist",
+    image: gespos,
   },
   {
     id: 4,
     title: "Swissforages",
     description: "Platform to acquire borehole data by Swisstopo",
+    image: swissforages,
   },
   {
     id: 5,
     title: "Mix Or Match game",
     description: "Memory game with a twist",
     url: "https://mali-mix-or-match.netlify.app/",
+    image: mixOrMatch,
   },
   {
     id: 6,
@@ -50,12 +69,14 @@ export const projectsData: ProjectsItemType[] = [
     description: "For the elderly and their caregivers",
     isApp: true,
     url: "https://apps.apple.com/us/app/connected-home-care/id1122799341",
+    image: connectedHomeCare,
   },
   {
     id: 7,
     title: "Cockpit Website",
     description: "For connecting passengers and drivers of the go! fleet",
     url: "https://go-fleet.ch/de/",
+    image: cockpit,
   },
   {
     id: 8,
@@ -63,6 +84,7 @@ export const projectsData: ProjectsItemType[] = [
     description: "For the drivers of the go! fleet",
     url: "https://go-fleet.ch/de/",
     isApp: true,
+    image: goFleet,
   },
   {
     id: 9,
@@ -70,12 +92,14 @@ export const projectsData: ProjectsItemType[] = [
     description: "For keep track of your car maintenance",
     url: "https://www.youtube.com/watch?v=U1StgHCsvyY",
     isApp: true,
+    image: autoTik,
   },
   {
     id: 10,
     title: "Limo Restaurant",
     description: "For ordering food online",
     url: "https://www.youtube.com/watch?v=8mu5ihADOlE",
+    image: limoRestaurant,
   },
   {
     id: 11,
@@ -83,5 +107,6 @@ export const projectsData: ProjectsItemType[] = [
     description: "Time Tracking App",
     url: "https://www.youtube.com/shorts/g4jDDvGodtY",
     isApp: true,
+    image: asena,
   },
 ];
